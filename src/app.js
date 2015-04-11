5d4f7914a0c202e7a7e8864ba560acf76b03d432
davidalim <14.d.lim@gmail.com>
2015-04-11 15:32:41 -0400

    constuctor for bs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 var ui = require('ui');
 var ws_setup = require('./ws_setup');
-var battle_scene = require('./battle_scene');
-var battle_brain = require('./battle_brain');
+var BattleScene = require('./battle_scene');
 
 var main = new ui.Card({
   body: 'Waiting for server data...'
 });
 main.show();
 //ws_setup();
-//battle_scene();
-battle_brain();
+
+var s = {name: 'PIKACHU', health: 10, image_back: 'images/25_back.png'};
+var o = {name: 'PIKACHU', health: 15, image_front: 'images/25_front.png'};
+var bs = new BattleScene(s, o);
+bs.display();
