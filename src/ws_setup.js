7b87f411e219b71aff67fadd3401db904347f2f8
hkgumbs <h.kofigumbs@gmail.com>
2015-04-12 03:57:05 -0400

    back to WebSockets

--- src/ws_setup.js
+++ src/ws_setup.js
@@ -1,38 +1,38 @@
 var Pokebelt = require('./pokebelt');
 var BattleScene = require('./battle_scene');
-var IO = require('./socket.io');
 
 function Client() {
   var self = this;
-  self.socket = IO('ws://43272bb7.ngrok.com');
+  self.socket = WebSocket('ws://pokemon-pebble.herokuapp.com');
   self.pokemon = Pokebelt.get(0);
 
   self.respond = function(data) {
     data.phase = 'ongoing';
-    self.socket.emit(self.id, data);
+    self.socket.send(JSON.stringify(data));
   };
 
   self.socket.on('message', function(data) {
     console.log(data);
-    switch(data.phase) {
+    var info = JSON.stringify(data);
+    switch(info.phase) {
       case 'handshake':
-        self.first = data.turn;
-        self.id = data.id;
+        self.first = info.turn;
+        self.id = info.id;
         var packet = {
           phase : 'init',
           pokemon : self.pokemon,
         };
-        self.socket.emit(self.id, packet);
+        self.socket.send(JSON.stringify(packet);
         break;
 
       case 'init':
-        var opponent = data.pokemon;
+        var opponent = info.pokemon;
         var callback = self.first ? self.respond : null;
         self.bs = new BattleScene(self.pokemon, opponent, callback);
         break;
 
       case 'ongoing':
-        self.bs.attack_screen(data, self.respond);
+        self.bs.attack_screen(info, self.respond);
         break;
       }
   };
